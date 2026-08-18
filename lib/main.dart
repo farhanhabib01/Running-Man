4c17955301e7cc456134b08f2cb3736e217c0c56
farhanhabib01 <mfarhanhabib776@gmail.com>
2026-08-18 14:57:33 +0500

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Runner Chase',
+      title: 'Runner Chase Final TEST',
       debugShowCheckedModeBanner: false,
       home: const GameScreen(),
     );
